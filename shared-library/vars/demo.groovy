b502f72338c62be0ecd8b15118725a7a93978fb3
hemanth <hemanthtadikonda29@gmail.com>
2023-12-31 17:08:33 +0530

    Jenkins "pipeline Script"

---
+++ shared-library/vars/demo.groovy
@@ -0,0 +1,40 @@
+def info(message) {
+   echo "INFO: ${message}"
+}
+
+def warning(message) {
+   echo "WARNING: ${message}"
+}
+
+def call() {
+   pipeline {
+      agent any
+      stages {
+         stage('compile'){
+            steps {
+               echo 'Hello World'
+            }
+         }
+         stage('Test'){
+            steps {
+               echo 'Hello World'
+            }
+         }
+         stage('code quality'){
+            steps {
+               echo 'Hello World'
+            }
+         }
+         stage('code security'){
+            steps {
+               echo 'Hello World'
+            }
+         }
+         stage('Release'){
+            steps {
+               echo 'Hello World'
+            }
+         }
+      }
+   }
+}
