743d8438119f848e05d621dd74073d55847bb30b
hemanth <hemanthtadikonda29@gmail.com>
2024-03-12 08:22:02 +0530

    jenkins pipeline

--- shared-library/vars/nodejs.groovy
+++ shared-library/vars/nodejs.groovy
@@ -20,16 +20,25 @@ def call() {
             }
          }
          stage('code quality'){
+            when {
+               expression { env.BRANCH_NAME ==~ "main" }
+            }
             steps{
                sh 'echo code quality'
             }
          }
          stage('code code security'){
+            when {
+               expression { env.BRANCH_NAME ==~ "main" }
+            }
             steps{
                sh 'echo code code security'
             }
          }
          stage('code Release'){
+            when {
+               expression { env.TAG_NAME ==~ ".*" }
+            }
             steps{
                sh 'echo code release'
             }
