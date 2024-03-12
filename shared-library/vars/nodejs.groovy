5cb18e550b06a060be0d5431282071551f815ec9
hemanth <hemanthtadikonda29@gmail.com>
2024-03-12 07:06:01 +0530

    jenkins pipeline

---
+++ shared-library/vars/nodejs.groovy
@@ -0,0 +1,33 @@
+def call() {
+   pipeline {
+      agent any
+      stages {
+         stage('code Complie') {
+            steps {
+               sh 'find .'
+               sh 'echo npm install'
+            }
+         }
+         stage('code Test'){
+            steps{
+               sh 'echo code test'
+            }
+         }
+         stage('code quality'){
+            steps{
+               sh 'echo code quality'
+            }
+         }
+         stage('code code security'){
+            steps{
+               sh 'echo code code security'
+            }
+         }
+         stage('code Release'){
+            steps{
+               sh 'echo code release'
+            }
+         }
+      }
+   }
+}
