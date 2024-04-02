f497d14e3884bf6f19b1153c34cb571959ea4c1e
hemanth <hemanthtadikonda29@gmail.com>
2024-04-02 19:01:43 +0530

    Configuration changes

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -1,9 +1,8 @@
 def compile() {
    stage('code complie'){
       if (env.codeType == "maven") {
-         sh 'mvn package'
-         //sh '/home/centos/maven/bin/mvn -version'
-         //sh '/root/maven/bin/mvn package'
+         sh '/home/centos/maven/bin/mvn package'
+
          //print 'maven'
       }
       if (env.codeType == "nodejs"){
